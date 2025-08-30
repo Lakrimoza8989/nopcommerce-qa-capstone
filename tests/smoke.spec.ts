0cffa2347febd3ca4ad4756fe32e8564b7b97578
Lakrimoza8989 <125205723+Lakrimoza8989@users.noreply.github.com>
2025-08-30 18:01:00 -0500

    Update smoke.spec.ts
    Test: add smoke test for homepage

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -3,8 +3,7 @@ import { test, expect } from '@playwright/test';
 test('homepage loads', async ({ page }) => {
   await page.goto('https://demo.nopcommerce.com/', { waitUntil: 'domcontentloaded' });
 
-  // Ждём стабильный элемент меню, который есть всегда
+  // ждём элемент меню "Computers"
   await expect(page.getByRole('link', { name: /computers/i }))
     .toBeVisible({ timeout: 30000 });
-
-
+});
