6700ad3717f2c0603008165e3b75730d0821bb48
Lakrimoza8989 <125205723+Lakrimoza8989@users.noreply.github.com>
2025-09-10 23:45:52 -0500

    Update smoke-search.spec.ts

--- tests/smoke-search.spec.ts
+++ tests/smoke-search.spec.ts
@@ -1,11 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('search returns results', async ({ page }) => {
-<<<<<<< HEAD
+
   await page.goto('https://nop-qa.portnov.com/');
-=======
   await page.goto('/');
->>>>>>> switch-to-portnov
   await page.getByPlaceholder('Search store').fill('book');
   await page.getByRole('button', { name: 'Search' }).click();
   await expect(page).toHaveURL(/\/search/);
