0664867f4b55d99d160fc5e0b1ccdad4ba474eff
Lakrimoza8989 <125205723+Lakrimoza8989@users.noreply.github.com>
2025-09-14 22:33:33 -0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,9 +11,10 @@ export default defineConfig({
     viewport: { width: 1366, height: 768 },
     userAgent:
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119 Safari/537.36',
-    screenshot: 'only-on-failure',
-    trace: 'retain-on-failure',
-    video: 'retain-on-failure',
+    screenshot: 'on',
+    trace: 'on',
+    video: 'on',
+
   },
   reporter: [
     ['list'],
